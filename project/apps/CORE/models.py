9a9c1fb77806deb9dd857fa91fafe8d1ce68d534
mati231087 <matias.cereghetti@icloud.com>
2023-07-10 00:01:01 -0300

    cargadas las clases

--- project/apps/CORE/models.py
+++ project/apps/CORE/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
 class Usuario(models.Model):
-    nombre_completo = models.CharField(max_length=100)
-    mail = models.EmailField()
-    telefono = models.CharField(max_length=20)
+    nombre_completo = models.CharField(max_length=100, default="sin nombre")
+    mail = models.EmailField(default="example@example.com")
+    telefono = models.CharField(max_length=20, default="sin telefono")
 
     def __str__(self):
         return self.nombre_completo
@@ -25,19 +26,26 @@ class Categoria(models.Model):
 
 
 class Producto(models.Model):
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    condicion = models.CharField(max_length=100)
-    eslora = models.DecimalField(max_digits=5, decimal_places=2)
-    manga = models.DecimalField(max_digits=5, decimal_places=2)
-    puntal = models.DecimalField(max_digits=5, decimal_places=2)
-    astillero = models.CharField(max_length=100)
-    anio_fabricacion = models.IntegerField()
-    motorizacion = models.CharField(max_length=100)
-    capacidad = models.IntegerField()
-    ubicacion = models.CharField(max_length=100)
-    estado = models.CharField(max_length=100)
-    comentario = models.TextField()
+    usuario = models.CharField(max_length=100, default="")
+    categoria = models.CharField(max_length=100, default="sin categoria")
+    condicion = models.CharField(max_length=100, default="sin condicion")
+    eslora = models.FloatField(default=0)
+    manga = models.FloatField(default=0)
+    puntal = models.FloatField(default=0)
+    astillero = models.CharField(max_length=100, default="sin astillero")
+    anio_fabricacion = models.IntegerField(
+    default=0,
+    validators=[
+        MinValueValidator(1900, 'El año de fabricación debe ser igual o mayor a 1900.'),
+        MaxValueValidator(timezone.now().year, 'El año de fabricación no puede ser mayor al año actual.')
+    ]
+)
+
+    motorizacion = models.CharField(max_length=100, default="sin motorizacion")
+    capacidad = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    ubicacion = models.CharField(max_length=100, default="sin ubicacion")
+    estado = models.CharField(max_length=100, default="sin estado")
+    comentario = models.TextField(max_length=1500, default="")
 
     def __str__(self):
         return f"{self.usuario} - {self.astillero} {self.anio_fabricacion}"
