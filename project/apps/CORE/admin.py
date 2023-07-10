e03c2931be6b619f8bd63ffa74bcba8f05a720cc
mati231087 <matias.cereghetti@icloud.com>
2023-07-09 22:55:05 -0300

    Generado el admin y models

--- project/apps/CORE/admin.py
+++ project/apps/CORE/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Categoria)
+admin.site.register(models.Producto)
+admin.site.register(models.Usuario)
--- project/apps/CORE/models.py
+++ project/apps/CORE/models.py
@@ -1,39 +1,47 @@
-
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
-class Usuario(AbstractUser):
-    username = models.CharField(max_length=150, unique=True)
-    password = models.CharField(
-        max_length=12,
-        validators=[
-            MinLengthValidator(8, 'La contraseña debe tener al menos 8 caracteres.'),
-            MaxLengthValidator(12, 'La contraseña no puede tener más de 12 caracteres.'),
-            RegexValidator(
-                regex='.*[0-9].*',
-                message='La contraseña debe contener al menos un número.'
-            )
-        ]
-    )
-
-    def __str__(self):
-        return self.username
-
-class Cliente(models.Model):
+class Usuario(models.Model):
     nombre_completo = models.CharField(max_length=100)
-    email = models.EmailField()
+    mail = models.EmailField()
     telefono = models.CharField(max_length=20)
-    direccion = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombre_completo
 
-class Producto(models.Model):
-    nombre = models.CharField(max_length=100)
-    descripcion = models.TextField()
-    foto = models.ImageField(upload_to='productos')
-    precio = models.DecimalField(max_digits=8, decimal_places=2)
+
+class Categoria(models.Model):
+    OPCIONES_CATEGORIA = [
+        ('motor', 'Embarcación a motor'),
+        ('vela', 'Embarcación a vela'),
+        ('moto_agua', 'Moto de agua'),
+        ('jet_ski', 'Jet Ski'),
+        ('remo', 'Embarcación a remo')
+    ]
+
+    nombre = models.CharField(max_length=50, choices=OPCIONES_CATEGORIA)
 
     def __str__(self):
         return self.nombre
+
+
+class Producto(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    condicion = models.CharField(max_length=100)
+    eslora = models.DecimalField(max_digits=5, decimal_places=2)
+    manga = models.DecimalField(max_digits=5, decimal_places=2)
+    puntal = models.DecimalField(max_digits=5, decimal_places=2)
+    astillero = models.CharField(max_length=100)
+    anio_fabricacion = models.IntegerField()
+    motorizacion = models.CharField(max_length=100)
+    capacidad = models.IntegerField()
+    ubicacion = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+    comentario = models.TextField()
+
+    def __str__(self):
+        return f"{self.usuario} - {self.astillero} {self.anio_fabricacion}"
+
+
+# Create your models here.
 
