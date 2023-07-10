5e740ff746646671740a10bbeb6946a30df14521
mati231087 <matias.cereghetti@icloud.com>
2023-07-10 12:39:31 -0300

    Agregado saludo y slogan al CORE

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Tercera-pre-entrega-Cereghetti
+
--- project/apps/CORE/views.py
+++ project/apps/CORE/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "CORE/index.html", {"Saludo": "Bienvenido a Entrenautas"})
+
+
+def index(request):
+    return render(request, "CORE/index.html", {"Slogan": "El primer mercado nautico digital"})
