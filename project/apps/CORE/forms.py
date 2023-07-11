2d1d7426618692dba0b97747737a12bd140c477f
mati231087 <matias.cereghetti@icloud.com>
2023-07-11 20:35:27 -0300

    last update

--- project/apps/CORE/forms.py
+++ project/apps/CORE/forms.py
@@ -7,13 +7,3 @@ class CategoriaForm(forms.ModelForm):
         model = Categoria
         fields = ["nombre"]
 
-class ProductoForm(forms.ModelForm):
-    class Meta:
-        model = Producto
-        fields = ["usuario", "categoria", "condicion", "eslora", "manga", "puntal", "astillero",
-                 "anio_fabricacion", "motorizacion", "capacidad", "ubicacion", "estado", "comentario"]
-
-class UsuarioForm(forms.ModelForm):
-    class Meta:
-        model = Usuario
-        fields = ['nombre_completo', 'mail', 'telefono']
