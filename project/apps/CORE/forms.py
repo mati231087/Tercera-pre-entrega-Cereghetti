a51b570ffeacb0fbace9586134657f98161d0ae7
mati231087 <matias.cereghetti@icloud.com>
2023-07-11 20:55:00 -0300

    update forms

--- project/apps/CORE/forms.py
+++ project/apps/CORE/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import Categoria
+from .models import Categoria, Producto, Usuario
 
 class CategoriaForm(forms.ModelForm):
     # Agrega los campos adicionales que deseas incluir en el formulario
@@ -10,3 +10,15 @@ class CategoriaForm(forms.ModelForm):
         model = Categoria
         fields = ["nombre", "campo_adicional"]
 
+class UsuarioForm(forms.ModelForm):
+
+    
+    class Meta:
+        model = Usuario
+        fields = ["nombre_completo", "mail", "telefono"]
+
+class ProductoForm(forms.ModelForm):
+
+    class Meta:
+        model = Producto
+        fields = ["usuario", "categoria", "condicion", "eslora", "manga", "puntal", "astillero", "anio_fabricacion", "motorizacion", "capacidad", "ubicacion", "estado", "comentario"]
