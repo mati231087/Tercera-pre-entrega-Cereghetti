b61faaf470b6e0d76e1ff8053a254bdbaa48ecd8
mati231087 <matias.cereghetti@icloud.com>
2023-07-11 20:46:10 -0300

    updted forms

--- project/apps/CORE/forms.py
+++ project/apps/CORE/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 
-from .models import Categoria, Producto, Usuario
+from .models import Categoria
 
 class CategoriaForm(forms.ModelForm):
+    # Agrega los campos adicionales que deseas incluir en el formulario
+    campo_adicional = forms.CharField(max_length=100, required=False)
+
     class Meta:
         model = Categoria
-        fields = ["nombre"]
+        fields = ["nombre", "campo_adicional"]
 
