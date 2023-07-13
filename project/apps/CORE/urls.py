f38b4a37fca2f807ae614a5dd5d60a252a97c42c
mati231087 <matias.cereghetti@icloud.com>
2023-07-13 19:09:41 -0300

    base urls.py

--- project/apps/CORE/urls.py
+++ project/apps/CORE/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 from . import views
-from .views import crear_categoria, crear_producto, crear_usuario, pagina_construccion
+from .views import crear_categoria, crear_producto, crear_usuario, pagina_construccion, base_view
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -10,5 +10,5 @@ urlpatterns = [
     path("cargar_producto/", crear_producto, name="cargar_producto"),
     path("crear-usuario/", crear_usuario, name="crear_usuario"),
     path("pagina-construccion/", pagina_construccion, name="pagina_construccion"),
-    path("base/", views.base_view, name="base"),  
+    path("base/", base_view, name="base"),  
 ]
